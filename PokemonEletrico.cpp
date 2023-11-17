328889ee0f4bb862d49e85d2373631ea264779b2
Felipe Martins <129700548+1felipemartins1@users.noreply.github.com>
2023-11-17 13:49:16 -0300

    pokemon eletricp

--- PokemonEletrico.cpp
+++ PokemonEletrico.cpp
@@ -1,6 +1,8 @@
-#include "PokemonEletrico.hpp"
+#include "Pokemon.hpp"
+#include "PokemonEletrico.hpp" 
 #include <iostream>
 #include <string>
+
 
 PokemonEletrico::PokemonEletrico(std::string nome, std::string tipo_ataque, double forca_ataque, double potencia_raio)
     : Pokemon(nome, tipo_ataque, forca_ataque), _potencia_raio(potencia_raio)
@@ -9,4 +11,18 @@ PokemonEletrico::PokemonEletrico(std::string nome, std::string tipo_ataque, doub
 
 void PokemonEletrico::falar_tipo_ataque()
 {
+    std::cout << this->_tipo_ataque << "!\n"
+    << "Bzzzz!" << std::endl;
+}
+
+double PokemonEletrico::calcular_dano(){
+    return ataque_eletrico(); 
+}
+
+
+
+
+double PokemonEletrico::ataque_eletrico(){
+    double dano = _forca_ataque * _potencia_raio; 
+    return dano; 
 }
